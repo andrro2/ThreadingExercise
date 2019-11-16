3565f613c440d0a558d2ceea68e9718ab60105dc
András Rozner <roznernek@gmail.com>
2019-11-16 13:42:18 +0100

    Excersize 2 finished

---
+++ SingleInstance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        public const string name = "RUNMEONLYONCE";
+
+        static void Main(string[] args)
+        {
+            Mutex mutexVar = null;
+            while (true)
+            {
+                try
+                {
+                    mutexVar = Mutex.OpenExisting(name);
+                    mutexVar.Close();
+                    Environment.Exit(0);
+
+                }
+                catch (WaitHandleCannotBeOpenedException e)
+                {
+                    mutexVar = new Mutex(true, name);
+                }
+            }
+        }
+    }
+}
